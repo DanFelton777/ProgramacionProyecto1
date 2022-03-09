5abbfbe2d43ab20aa73e83d4360c89ecb1136bb0
DanFelton <dani_@DESKTOP-INTS0C0>
2022-03-09 14:09:07 -0600

    Tengo una idea pero no ando muy segura, se junta lo del programa abuelo y el de animales, pero me da erroes en los nombres

---
+++ Proyecto1_HarryPotter/src/main/java/Personajes/Probar_personajes.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personajes;
+
+/**
+ *
+ * @autores Daniela Ramos Salazar (20280827) && Irving Alexis Nava Flores (20280864)
+ */
+
+public class Probar_personajes {
+    
+    public static void main(String[] args) {
+  //Nombre junto xd, especie, nacionalidad, año de nacimiento (da la edad respecto al año )//
+
+   //Personaje personaje1= newPersonaje (Harry Potter,mago,britanico,1997); 
+   //Personaje personaje2= newPersonaje (ViktorKrum,mago,bulgara,1997); 
+   
+
+    
+    }     
+}
