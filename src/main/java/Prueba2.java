21f97834ebd7cb33f9339cd97f1128bef465c85f
DanFelton <dani_@DESKTOP-INTS0C0>
2022-03-09 14:55:38 -0600

    Sombrero seleccionador

---
+++ src/main/java/Prueba2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dani_
+ */
+public class Prueba2 {
+       
+    public static void main(String[] args) {
+    
+    System.out.println("Puedes pertenecer a Gryffindor,\n" +
+"Donde habitan los valientes.\n" +
+"Su osadía, temple y caballerosidad\n" +
+"Ponen aparte a los de Gryffindor.\n" +
+"Puedes pertenecer a Hufflepuff,\n" +
+"Donde son justos y leales.\n" +
+"Esos perseverantes Hufflepuff\n" +
+"De verdad no temen el trabajo pesado.\n" +
+"O tal vez a la antigua sabiduría de Ravenclaw,\n" +
+"Si tienes una mente dispuesta,\n" +
+"Porque los de inteligencia y erudición\n" +
+"Siempre encontrarán allí a sus semejantes.\n" +
+"O tal vez en Slytherin\n" +
+"Harás tus verdaderos amigos.\n" +
+"Esa gente astuta utiliza cualquier medio\n" +
+"Para lograr sus fines.");
+  
+    }
+}
+
